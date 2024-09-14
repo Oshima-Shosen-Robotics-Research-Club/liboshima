6bca8c0fe2fe841afb6f3fb34caf999de01e0e54
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-09-14 13:05:52 +0900

    Update ImReceiver.ino to use SoftwareSerial for debugging

--- examples/ImReceiver/ImReceiver.ino
+++ examples/ImReceiver/ImReceiver.ino
@@ -1,6 +1,8 @@
 #include <components/ims/ImReceiver.h>
+#include <SoftwareSerial.h>
 
 ImReceiver imReceiver(Serial);
+SoftwareSerial debugSerial(0, 0);
 
 void setup() {}
 
@@ -8,6 +10,6 @@ void loop() {
   // ImSender.inoから送信されたデータを受信する
   int data;
   if (imReceiver.receive(data)) {
-    Serial.println(data);
+    debugSerial.println(data);
   }
 }
