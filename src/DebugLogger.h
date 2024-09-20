82174740b0d4c42c14995a2c37f72642077a8d25
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-09-21 01:02:48 +0900

    Create DebugLogger class to handle all serial communications using SerialPort class

---
+++ src/DebugLogger.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "serials/SerialPort.h"
+#include <Arduino.h>
+
+#if defined(DEBUG)
+/**
+ * @brief デバッグメッセージをシリアルポートに出力するクラス
+ *
+ * `DebugLogger`クラスは、ハードウェアまたはソフトウェアのシリアルポートにデバッグメッセージを出力するためのメソッドを提供します。
+ * シンプルなメッセージやフォーマットされたメッセージの出力がサポートされています。
+ *
+ * デバッグメッセージを出力するためには、`DEBUG`マクロを定義し、`init`メソッドを呼び出してシリアルポートを初期化する必要があります。
+ */
+class DebugLogger {
+public:
+  /**
+   * @brief ハードウェアシリアルポートで `DebugLogger` を初期化します。
+   *
+   * @param serial ログ出力に使用するハードウェアシリアルポート（例: Serial）。
+   * @param baudrate シリアル通信のボーレート（デフォルトは 19200）。
+   */
+  void init(SerialPort &serial, unsigned long baudrate = 19200);
+
+  /**
+   * @brief シンプルなデバッグメッセージをシリアルポートに出力します。
+   *
+   * @param className ログが呼び出されるクラスの名前。
+   * @param methodName ログが呼び出されるメソッドの名前。
+   * @param message 出力するデバッグメッセージ。
+   */
+  void println(const char *className, const char *methodName,
+               const char *message);
+
+  /**
+   * @brief フォーマットされたデバッグメッセージをシリアルポートに出力します。
+   *
+   * @param className ログが呼び出されるクラスの名前。
+   * @param methodName ログが呼び出されるメソッドの名前。
+   * @param format メッセージのフォーマット文字列（printf 形式）。
+   * @param ... メッセージをフォーマットするための可変引数。
+   */
+  void printlnf(const char *className, const char *methodName,
+                const char *format, ...);
+
+private:
+  /**
+   * @brief ログ出力に使用するシリアルポートへのポインタ。
+   */
+  SerialPort *serialPort;
+};
+
+extern DebugLogger Debug; ///< グローバルインスタンスの宣言
+
+#else // DEBUG is not defined
+class DebugLogger {
+public:
+  void init(SerialPort &serial, unsigned long baudrate = 19200) {}
+  void println(const char *className, const char *methodName,
+               const char *message) {}
+  void printlnf(const char *className, const char *methodName,
+                const char *format, ...) {}
+};
+
+extern DebugLogger Debug; ///< グローバルインスタンスの宣言
+
+#endif // DEBUG
