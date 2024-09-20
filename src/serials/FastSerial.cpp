101738688ee750b0e7e1efc5866baf419e359662
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-09-21 02:59:33 +0900

    Update FastSerial class to support printing integers and new line characters

--- src/serials/FastSerial.cpp
+++ src/serials/FastSerial.cpp
@@ -1,42 +1,29 @@
 #include "FastSerial.h"
 
-#if defined(__AVR_ATmega328P__)
 FastSerial FSerial;
 
-// 静的メンバ変数の定義
+#if defined(__AVR_ATmega328P__)
 void (*FastSerial::user_onReceive)(void) = NULL;
 
 void FastSerial::begin(unsigned long baudrate) {
-  // ボーレートの設定
   UBRR0 = (F_CPU / 16 / baudrate - 1);
-
-  // 送受信機能と割り込みを有効化
   UCSR0B = _BV(RXEN0) | _BV(TXEN0) | _BV(RXCIE0);
-
-  // データフォーマット設定（8ビットデータ、1ストップビット、パリティなし）
   UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
-
-  // 全割り込みを有効化
   sei();
 }
 
-size_t FastSerial::available() {
-  // 受信データがあるかどうかをチェック
-  return UCSR0A & _BV(RXC0);
-}
+uint8_t FastSerial::available() { return UCSR0A & _BV(RXC0); }
 
 char FastSerial::read() {
-  // データが受信されるまで待つ
   while (!available())
     ;
   return UDR0;
 }
 
-size_t FastSerial::readBytesUntil(char terminator, char *buffer,
-                                  size_t length) {
-  size_t count = 0;
+uint8_t FastSerial::readBytesUntil(char terminator, char *buffer,
+                                   uint8_t length) {
+  uint8_t count = 0;
   while (count < length) {
-    // データが受信されるまで待つ
     while (!available())
       ;
     buffer[count] = read();
@@ -48,21 +35,34 @@ size_t FastSerial::readBytesUntil(char terminator, char *buffer,
   return count;
 }
 
-size_t FastSerial::print(const char *str) {
-  size_t count = 0;
+uint8_t FastSerial::write(uint8_t data) {
+  while (!(UCSR0A & _BV(UDRE0)))
+    ;
+  UDR0 = data;
+  return 1;
+}
+
+uint8_t FastSerial::print(const char *str) {
+  uint8_t count = 0;
   while (*str) {
-    // 送信バッファが空になるまで待つ
-    while (!(UCSR0A & _BV(UDRE0)))
-      ;
-    UDR0 = *str++;
+    write(*str++);
     count++;
   }
   return count;
 }
 
-size_t FastSerial::println(const char *str) {
-  size_t count = print(str);
-  count += print("\r\n"); // 改行を追加
+uint8_t FastSerial::println(const char *str) {
+  uint8_t count = print(str);
+  count += println();
+  return count;
+}
+
+uint8_t FastSerial::println() { return print("\r\n"); }
+
+uint8_t FastSerial::print(uint8_t value) {
+  uint8_t count = 0;
+  write(lookup[value >> 4] << 4 | lookup[value & 0x0F]);
+  count += 2;
   return count;
 }
 
@@ -70,10 +70,9 @@ void FastSerial::onReceive(void (*function)(void)) {
   user_onReceive = function;
 }
 
-// USART 受信完了割り込みハンドラ
 ISR(USART_RX_vect) {
   if (FastSerial::user_onReceive) {
-    FastSerial::user_onReceive(); // ユーザー定義のコールバック関数を呼び出す
+    FastSerial::user_onReceive();
   }
 }
 #endif
--- src/serials/FastSerial.h
+++ src/serials/FastSerial.h
@@ -1,36 +1,24 @@
 #pragma once
 
 #include <avr/interrupt.h>
+#include <avr/io.h>
 #include <stddef.h>
 #include <stdint.h>
 
-// ATmega328Pの場合
-#if defined(__AVR_ATmega328P__)
 class FastSerial {
 public:
-  // シリアル通信の初期化
   void begin(unsigned long baudrate);
-
-  // 受信バッファにデータがあるかどうかをチェック
-  size_t available();
-
-  // 1バイトのデータを受信
+  uint8_t available();
   char read();
-
-  // 指定した文字が受信されるまでデータを読み込む
-  size_t readBytesUntil(char terminator, char *buffer, size_t length);
-
-  // 文字列を送信
-  size_t print(const char *str);
-
-  // 文字列を送信して改行
-  size_t println(const char *str);
-
-  // ユーザー定義のコールバック関数を登録
+  uint8_t readBytesUntil(char terminator, char *buffer, uint8_t length);
+  uint8_t write(uint8_t data);
+  uint8_t print(const char *str);
+  uint8_t print(uint8_t value);
+  uint8_t println(const char *str);
+  uint8_t println();
   void onReceive(void (*function)(void));
-
   static void (*user_onReceive)(void);
 };
 
-extern FastSerial FSerial;
-#endif
+const char lookup[] = {'0', '1', '2', '3', '4', '5', '6', '7',
+                       '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
