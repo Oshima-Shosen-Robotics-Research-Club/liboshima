da1c813914e35e6b1cc14922a504c512734e3675
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-09-21 18:58:06 +0900

    Add necessary include paths for components and utilities

---
+++ FArduino.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <components/LedTape.h>
+#include <components/ims/ImReceiver.h>
+#include <components/ims/ImSender.h>
+#include <components/motors/BD62193.h>
+#include <components/motors/MotorDriver.h>
+#include <components/motors/TB67H450.h>
+#include <components/servos/HS_485HB.h>
+#include <components/servos/MG90S.h>
+#include <digitalWriteFast.h>
+#include <serials/FastSerial.h>
+#include <serials/SerialPort.h>
+#include <utils/Converter.h>
+#include <utils/DebugLogger.h>
