5defff62762f622bef28517298d9867025d7cfd2
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-09-22 23:09:10 +0900

    Remove unused code and update main function in Fastino.cpp

--- src/Fastino.h
+++ src/Fastino.h
@@ -1,20 +1,15 @@
 #pragma once
 
-#include <components/LedTape.h>
-#include <components/ims/ImReceiver.h>
-#include <components/ims/ImSender.h>
-#include <components/motors/BD62193.h>
-#include <components/motors/MotorDriver.h>
-#include <components/motors/TB67H450.h>
-#include <components/servos/HS_485HB.h>
-#include <components/servos/MG90S.h>
-#include <digitalWriteFast.h>
-#include <serials/FastwareSerial.h>
-#include <serials/SerialPort.h>
-#include <utils/Converter.h>
-#include <utils/DebugLogger.h>
+#include <stddef.h>
+#include <stdint.h>
 
-void setup();
-void loop();
-extern SerialPort FSerialPort;
-extern SerialPort HSerialPort;
+inline void setup();
+inline void loop();
+
+int main() {
+  setup();
+  while (true) {
+    loop();
+  }
+  return 0;
+}
