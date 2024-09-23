841729731fe5c509dfac6cd3bcce6a059724668d
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-09-23 16:00:18 +0900

    update comments

--- src/utils/Converter.h
+++ src/utils/Converter.h
@@ -1,19 +1,66 @@
+/**
+ * @file Converter.h
+ * @brief バイナリデータと16進数表現の相互変換を行うクラス
+ *
+ * `Converter` クラスは、バイトデータを16進数表現に変換したり、
+ * 16進数表現からバイトデータに変換する機能を提供します。
+ */
+
 #pragma once
 
 #include <stddef.h>
 #include <stdint.h>
 
+/**
+ * @class Converter
+ * @brief バイナリデータと16進数表現の相互変換を行うユーティリティクラス
+ *
+ * `Converter` クラスは、データを16進数表現に変換する静的メソッドや、
+ * 16進数表現から元のバイトデータに変換するメソッドを提供します。
+ */
 class Converter {
 public:
-    // 16進数表現に変換
-    static void toHex(const uint8_t* data, size_t length, char* output);
+  /**
+   * @brief バイトデータを16進数表現に変換します。
+   *
+   * 指定されたバイトデータを16進数表現に変換し、出力バッファに格納します。
+   * 各バイトは2文字の16進数表現に変換されます。
+   *
+   * @param data 変換するバイトデータのポインタ
+   * @param length バイトデータの長さ
+   * @param output
+   * 変換後の16進数文字列を格納するバッファ（null終端文字を含めた十分なサイズが必要です）
+   *
+   * @note `output` バッファのサイズは `length * 2 + 1` バイト必要です。
+   */
+  static void toHex(const uint8_t *data, size_t length, char *output);
 
-    // 16進数表現からバイトデータに変換
-    static void fromHex(const char* hexString, size_t length, uint8_t* output);
+  /**
+   * @brief 16進数表現からバイトデータに変換します。
+   *
+   * 指定された16進数文字列をバイトデータに変換し、出力バッファに格納します。
+   * 各2文字の16進数表現が1バイトに変換されます。
+   *
+   * @param hexString 変換する16進数文字列のポインタ
+   * @param length 16進数文字列の長さ（2の倍数でなければなりません）
+   * @param output 変換後のバイトデータを格納するバッファ
+   *
+   * @note `length` は偶数でなければなりません。奇数の場合の動作は未定義です。
+   */
+  static void fromHex(const char *hexString, size_t length, uint8_t *output);
 
 private:
-    // 16進数用の変換表 (0-9, A-F)
-    static const char hexLookup[16];
-    // 16進数文字列から数値への変換表
-    static const uint8_t valueLookup[23];
+  /**
+   * @brief 16進数文字に対応する数値を取得するためのルックアップテーブル
+   *
+   * '0'-'9' 及び 'A'-'F' を対応する16進数の値に変換するためのテーブルです。
+   */
+  static const char hexLookup[16];
+
+  /**
+   * @brief 16進数文字列の各文字を数値に変換するためのルックアップテーブル
+   *
+   * '0'-'9' と 'A'-'F' を、それぞれ対応する数値に変換するためのテーブルです。
+   */
+  static const uint8_t valueLookup[23];
 };
--- src/utils/DebugLogger.h
+++ src/utils/DebugLogger.h
@@ -5,61 +5,103 @@
 
 #if defined(DEBUG)
 /**
+ * @class DebugLogger
  * @brief デバッグメッセージをシリアルポートに出力するクラス
  *
- * `DebugLogger`クラスは、ハードウェアまたはソフトウェアのシリアルポートにデバッグメッセージを出力するためのメソッドを提供します。
- * シンプルなメッセージやフォーマットされたメッセージの出力がサポートされています。
+ * `DebugLogger`
+ * クラスは、ハードウェアまたはソフトウェアのシリアルポートを使用して、デバッグメッセージを出力するための機能を提供します。
+ * クラス名やメソッド名を指定してシンプルなログメッセージやフォーマットされたメッセージを出力することができます。
  *
- * デバッグメッセージを出力するためには、`DEBUG`マクロを定義し、`init`メソッドを呼び出してシリアルポートを初期化する必要があります。
+ * このクラスのメソッドは `DEBUG` マクロが定義されている場合にのみ機能します。
  */
 class DebugLogger {
 public:
   /**
-   * @brief ハードウェアシリアルポートで `DebugLogger` を初期化します。
+   * @brief シリアルポートで `DebugLogger` を初期化します。
    *
-   * @param serial ログ出力に使用するハードウェアシリアルポート（例: Serial）。
+   * このメソッドは、指定されたシリアルポートとボーレートを使ってログの出力を行う準備をします。
+   *
+   * @param serial ログ出力に使用する `SerialPort` インスタンス（例:
+   * `Serial`）。
    * @param baudrate シリアル通信のボーレート（デフォルトは 19200）。
    */
   void init(SerialPort &serial, unsigned long baudrate = 19200);
 
   /**
-   * @brief シンプルなデバッグメッセージをシリアルポートに出力します。
+   * @brief シンプルなデバッグメッセージを出力します。
    *
-   * @param className ログが呼び出されるクラスの名前。
-   * @param methodName ログが呼び出されるメソッドの名前。
+   * このメソッドは、指定されたクラス名、メソッド名、メッセージをシリアルポートに出力します。
+   * デバッグ用にシンプルなメッセージを提供したい場合に使用します。
+   *
+   * @param className 呼び出し元クラスの名前。
+   * @param methodName 呼び出し元メソッドの名前。
    * @param message 出力するデバッグメッセージ。
    */
   void println(const char *className, const char *methodName,
                const char *message);
 
   /**
-   * @brief フォーマットされたデバッグメッセージをシリアルポートに出力します。
+   * @brief フォーマットされたデバッグメッセージを出力します。
    *
-   * @param className ログが呼び出されるクラスの名前。
-   * @param methodName ログが呼び出されるメソッドの名前。
-   * @param format メッセージのフォーマット文字列（printf 形式）。
-   * @param ... メッセージをフォーマットするための可変引数。
+   * このメソッドは、指定されたクラス名、メソッド名と共に、可変引数を使用してフォーマットされたメッセージをシリアルポートに出力します。
+   * `printf`
+   * 形式でメッセージを指定できるため、動的な内容を含むメッセージを出力する際に便利です。
+   *
+   * @param className 呼び出し元クラスの名前。
+   * @param methodName 呼び出し元メソッドの名前。
+   * @param format メッセージのフォーマット文字列（`printf` 形式）。
+   * @param ... メッセージのフォーマットに使用する可変引数。
    */
   void printlnf(const char *className, const char *methodName,
                 const char *format, ...);
 
 private:
   /**
-   * @brief ログ出力に使用するシリアルポートへのポインタ。
+   * @brief ログの出力に使用するシリアルポートへのポインタ。
    */
   SerialPort *serialPort;
 };
 
 #else // DEBUG is not defined
+/**
+ * @class DebugLogger
+ * @brief デバッグモードが無効な場合のダミークラス
+ *
+ * `DEBUG`
+ * が定義されていない場合、このクラスは機能しないダミーメソッドを持ち、パフォーマンスへの影響を最小限に抑えます。
+ */
 class DebugLogger {
 public:
+  /**
+   * @brief ダミーの初期化メソッド
+   *
+   * `DEBUG` が定義されていない場合、このメソッドは何もしません。
+   */
   void init(SerialPort &serial, unsigned long baudrate = 19200) {}
+
+  /**
+   * @brief ダミーのメッセージ出力メソッド
+   *
+   * `DEBUG` が定義されていない場合、このメソッドは何もしません。
+   */
   void println(const char *className, const char *methodName,
                const char *message) {}
+
+  /**
+   * @brief ダミーのフォーマットメッセージ出力メソッド
+   *
+   * `DEBUG` が定義されていない場合、このメソッドは何もしません。
+   */
   void printlnf(const char *className, const char *methodName,
                 const char *format, ...) {}
 };
 
 #endif // DEBUG
 
-extern DebugLogger Logger; ///< グローバルインスタンスの宣言
+/**
+ * @brief グローバル `DebugLogger` インスタンス
+ *
+ * プロジェクト全体で使用するデフォルトの `DebugLogger` インスタンス。
+ * `DEBUG` が有効な場合にログを出力します。
+ */
+extern DebugLogger Logger;
