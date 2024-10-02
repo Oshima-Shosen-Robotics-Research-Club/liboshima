71cd48b03e63729a4ec34b60d078bc50b0ebbddc
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-10-02 18:17:30 +0900

    サイズが0の時に発生するコンパイルエラーを修正

--- src/components/Controller.h
+++ src/components/Controller.h
@@ -16,20 +16,14 @@
 /**
  * @brief コントローラ構造体
  *
- * @tparam numMotors モーターの数（デフォルトは4）
+ * @tparam numMotors モーターの数（デフォルトは0）
  * @tparam numButtons ボタンの数（デフォルトは0）
  * @tparam numSticks スティックの数（デフォルトは0）
  *
  * この構造体は、モーター、ボタン、スティックの状態を管理します。
  */
-template <uint8_t numMotors = 4, uint8_t numButtons = 0, uint8_t numSticks = 0>
+template <uint8_t numMotors = 0, uint8_t numButtons = 0, uint8_t numSticks = 0>
 struct Controller {
-  /**
-   * @brief ボタンの状態を格納する変数
-   *
-   * ボタンの状態をビットフィールドとして格納します。
-   */
-  uint32_t buttons : numButtons;
 
   /**
    * @brief モーター構造体
@@ -37,9 +31,16 @@ struct Controller {
    * モーターのピン状態を管理します。
    */
   struct Motor {
-    uint8_t pin1 : 1;  ///< ピン1の状態（1ビット）
-    uint8_t pin2 : 1;  ///< ピン2の状態（1ビット）
-  } motors[numMotors]; ///< モーターの配列
+    uint8_t pin1 : 1;      ///< ピン1の状態（1ビット）
+    uint8_t pin2 : 1;      ///< ピン2の状態（1ビット）
+  } motors[numMotors + 1]; ///< モーターの配列
+
+  /**
+   * @brief ボタンの状態を格納する変数
+   *
+   * ボタンの状態をビットフィールドとして格納します。
+   */
+  uint32_t buttons : numButtons + 1;
 
   /**
    * @brief スティック構造体
@@ -47,9 +48,9 @@ struct Controller {
    * スティックのX軸とY軸の状態を管理します。
    */
   struct Stick {
-    uint8_t x;         ///< X軸の状態
-    uint8_t y;         ///< Y軸の状態
-  } sticks[numSticks]; ///< スティックの配列
+    uint8_t x;             ///< X軸の状態
+    uint8_t y;             ///< Y軸の状態
+  } sticks[numSticks + 1]; ///< スティックの配列
 };
 
 #pragma pack(pop)
