15a32e27ee5a4971f0be3664c9babc6438d6c5e7
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-10-04 22:28:13 +0900

    Add Doxygen

--- src/components/motors/NonSpeedAdjustable.h
+++ src/components/motors/NonSpeedAdjustable.h
@@ -2,6 +2,14 @@
 
 #include <Arduino.h>
 
+/**
+ * @class NonSpeedAdjustable
+ * @brief A class to control a motor without speed adjustment capabilities.
+ * 
+ * This class provides basic functionalities to control a motor's direction
+ * (forward and reverse) and to stop the motor. It does not support speed
+ * adjustment.
+ */
 class NonSpeedAdjustable {
 public:
   NonSpeedAdjustable(uint8_t pin1, uint8_t pin2);
