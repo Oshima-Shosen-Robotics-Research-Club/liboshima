32066730fada095b59ac70f623128cce0f7f51e7
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-10-04 12:07:49 +0900

    Add consistency check workflow for library.json and library.properties

---
+++ scripts/common.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+get_version_from_json() {
+  jq -r '.version' library.json
+}
+
+get_version_from_properties() {
+  grep -E '^version=' library.properties | sed 's/version=//'
+}
+
+get_name_from_json() {
+  jq -r '.name' library.json
+}
+
+get_name_from_properties() {
+  grep -E '^name=' library.properties | sed 's/name=//'
+}
+
+get_dependencies_from_json() {
+  jq -r '.dependencies | to_entries[] | "\(.key)=\(.value)"' library.json
+}
+
+get_dependencies_from_properties() {
+  grep -E '^depends=' library.properties | sed 's/depends=//'
+}
