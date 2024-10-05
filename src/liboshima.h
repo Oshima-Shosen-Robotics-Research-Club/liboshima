f04af28a104e3828cc77ca0224f0374b1e2bd659
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-10-06 01:04:41 +0900

    Refactor include paths in liboshima.h

--- src/liboshima.h
+++ src/liboshima.h
@@ -3,16 +3,16 @@
 /**
  * @file liboshima.h
  * @brief Arduino IDE用のヘッダーファイル
- * 
+ *
  * Arduino IDEではサブディレクトリが認識されないから。
  * 例：#include <components\ims\IM920SL.h>はArduino IDEではできない。
  */
 
-#include "components/Controller.h"
-#include "components/ims/IM920SL.h"
-#include "components/ims/ImReceiver.h"
-#include "components/ims/ImSender.h"
 #include "components/LedTape.h"
+#include "components/controllers/Controller.h"
+#include "components/ims/IM920SL.h"
+#include "components/ims/ImReceiver_private.h"
+#include "components/ims/ImSender_private.h"
 #include "components/motors/NonSpeedAdjustable.h"
 #include "components/motors/speed/BD62193.h"
 #include "components/motors/speed/ISpeedAdjustable.h"
