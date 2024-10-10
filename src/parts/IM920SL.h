37280d7324dd56a1373bbda50b03ef0761d4d51b
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-10-10 22:18:45 +0900

    修正: IM920SLクラスにImSenderとImReceiverを統合

--- src/parts/IM920SL.h
+++ src/parts/IM920SL.h
@@ -1,18 +1,28 @@
 /**
- * @file ImReceiver.h
- * @brief IM920SL受信クラス
+ * @file IM920SL.h
  *
- * このファイルには、IM920SLモジュールからデータを受信するための `ImReceiver`
- * クラスが定義されています。
- * SerialTypeとLoggerTypeをテンプレート引数として受け取ります。
+ * @brief IM920SLモジュールを使用してデータの送受信を行うためのクラス定義
+ *
+ * このファイルには、IM920SLモジュールを使用してデータの送受信を行うための
+ * `IM920SL` クラスが定義されています。
+ * このクラスは、IM920SLモジュールを使用してデータの送受信を行うための
+ * 機能を提供します。
  */
 #pragma once
 
+#include <Arduino.h>
 #include <types/IsSame.h>
 #include <utils/Converter.h>
 #include <utils/DebugLogger.h>
 
+#define IM_SEND_INTERVAL 60
 #define IM_RECEIVE_TIMEOUT 1000 ///< 受信タイムアウト時間（ミリ秒）
+
+enum class ImSenderMode : uint8_t {
+  BUFFER_FULL,  ///< バッファがいっぱいの場合は即座に終了
+  CAREER_SENSE, ///< キャリアセンスを検出するまで待機
+  NO_WAIT       ///< データが利用できない場合は即座に終了
+};
 
 enum class ImReceiverMode : uint8_t {
   WAIT,   ///< データが利用可能になるまで待機
@@ -20,15 +30,15 @@ enum class ImReceiverMode : uint8_t {
 };
 
 /**
- * @brief IM920SL受信クラス
+ * @brief IM920SLクラス
  *
- * このクラスは、IM920SLモジュールからデータを受信するためのクラスです。
+ * このクラスは、IM920SLモジュールを使用してデータの送受信を行うためのクラスです。
  * SerialTypeとLoggerTypeをテンプレート引数として受け取ります。
  *
  * @tparam SerialType シリアル通信の型
- * @tparam LoggerType ロガーの型（デフォルトはDebugLogger<void>*）
+ * @tparam LoggerType ロガーの型（デフォルトはDebugLogger<SerialType>*）
  */
-template <typename SerialType, typename LoggerType> class ImReceiver {
+template <typename SerialType, typename LoggerType = void> class IM920SL {
 public:
   /**
    * @brief コンストラクタ
@@ -36,8 +46,56 @@ public:
    * @param serial シリアル通信オブジェクト
    * @param logger ロガーオブジェクト（デフォルトはnullptr）
    */
-  ImReceiver(SerialType &serial, LoggerType *logger = nullptr)
-      : serial(serial), logger(logger) {}
+  IM920SL(SerialType &serial, LoggerType *logger = nullptr)
+      : serial(serial), logger(logger) {
+    static_assert(!IsSame<DebugLogger<SerialType>, LoggerType>::value,
+                  "シリアル通信がロガーと競合しています");
+  }
+
+  /**
+   * @brief シリアル通信を開始する
+   *
+   * @param baudrate ボーレート（デフォルトは19200）
+   */
+  void begin(unsigned long baudrate = 19200) { serial.begin(baudrate); }
+
+  /**
+   * @brief データを送信するテンプレート関数
+   *
+   * @tparam T 送信するデータの型
+   * @param data 送信するデータ
+   */
+  template <typename T> void send(const T &data, ImSenderMode waitmode) {
+
+    static_assert(
+        sizeof(T) >= 1 && sizeof(T) <= 32,
+        "送信する型のサイズは1バイト以上32バイト以下である必要があります");
+
+    printLog(DebugLoggerLevel::INFO, "send", "Sending data");
+
+    constexpr uint8_t size = 5 + (sizeof(T) * 2);
+    if (static_cast<uint8_t>(serial.availableForWrite()) < size) {
+      if (waitmode == ImSenderMode::NO_WAIT) {
+        return;
+      } else if (waitmode == ImSenderMode::BUFFER_FULL) {
+        while (static_cast<uint8_t>(serial.availableForWrite()) < size)
+          ;
+      }
+    }
+
+    if (waitmode == ImSenderMode::CAREER_SENSE) {
+      delay(IM_SEND_INTERVAL);
+    }
+
+    serial.print("TXDA ");
+    for (uint8_t i = 0; i < sizeof(T); i++) {
+      serial.print(reinterpret_cast<const uint8_t *>(&data)[i] >> 4 & 0xF, HEX);
+      serial.print(reinterpret_cast<const uint8_t *>(&data)[i] & 0xF, HEX);
+    }
+    serial.println();
+
+    printLog(DebugLoggerLevel::INFO, "send", "Data sent");
+  }
 
   /**
    * @brief データを受信するテンプレート関数
@@ -52,6 +110,7 @@ public:
     static_assert(
         sizeof(T) >= 1 && sizeof(T) <= 32,
         "受信する型のサイズは1バイト以上32バイト以下である必要があります");
+        
     printLog(DebugLoggerLevel::INFO, "receive", "Receiving data");
 
     // コロン以前の文字列を読み捨てる
@@ -117,7 +176,7 @@ private:
   inline void printLog(DebugLoggerLevel level, const char *methodName,
                        const char *message) {
     if constexpr (!IsSame<LoggerType, void>::value) {
-      logger->println(level, DebugLoggerMode::WAIT, "ImReceiver", methodName,
+      logger->println(level, DebugLoggerMode::WAIT, "IM920SL", methodName,
                       message);
     }
   }
@@ -127,7 +186,7 @@ private:
     if constexpr (!IsSame<LoggerType, void>::value) {
       va_list args;
       va_start(args, format);
-      logger->printlnf(level, DebugLoggerMode::WAIT, "ImReceiver", methodName,
+      logger->printlnf(level, DebugLoggerMode::WAIT, "IM920SL", methodName,
                        format, args);
       va_end(args);
     }
