256b4c2a73743aae4131d982b930101d833d6f7d
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-10-11 12:42:50 +0900

    修正: inlined

--- src/parts/Button.cpp
+++
@@ -1,7 +0,0 @@
-#include "Button.h"
-#include <Arduino.h>
-#include <digitalWriteFast.h>
-
-Button::Button(uint8_t pin) : pin(pin) { pinModeFast(pin, INPUT_PULLUP); }
-
-bool Button::isPressed() { return !digitalReadFast(pin); }
--- src/parts/Button.h
+++ src/parts/Button.h
@@ -1,11 +1,13 @@
 #pragma once
 
+#include <Arduino.h>
+#include <digitalWriteFast.h>
 #include <stdint.h>
 
 class Button {
 public:
-  Button(uint8_t pin);
-  bool isPressed();
+  Button(uint8_t pin) : pin(pin) { pinModeFast(pin, INPUT_PULLUP); }
+  bool isPressed() { return digitalReadFast(pin) == LOW; }
 
 private:
   uint8_t pin;
